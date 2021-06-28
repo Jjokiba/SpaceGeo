c68ab875de01acc2bbb07c730272eca505272f55
Joao Casagrande <pngbighouse@gmail.com>
2021-06-28 20:41:01 -0300

    incremento

--- CalculadoraGeometrica/Forms/frmPrincipal.Designer.cs
+++ CalculadoraGeometrica/Forms/frmPrincipal.Designer.cs
@@ -34,62 +34,79 @@ namespace CalculadoraGeometrica.Forms
             this.cmbForma = new System.Windows.Forms.ComboBox();
             this.btnSelecionar = new System.Windows.Forms.Button();
             this.btnAdicionar = new System.Windows.Forms.Button();
+            this.Hiacaco = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(99, 28);
+            this.label1.Location = new System.Drawing.Point(74, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(169, 17);
+            this.label1.Size = new System.Drawing.Size(129, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Bem vindo ao Space Geo";
             // 
             // picImage
             // 
-            this.picImage.Location = new System.Drawing.Point(12, 84);
+            this.picImage.Location = new System.Drawing.Point(9, 68);
+            this.picImage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.picImage.Name = "picImage";
-            this.picImage.Size = new System.Drawing.Size(348, 247);
+            this.picImage.Size = new System.Drawing.Size(261, 201);
             this.picImage.TabIndex = 2;
             this.picImage.TabStop = false;
             // 
             // cmbForma
             // 
             this.cmbForma.FormattingEnabled = true;
-            this.cmbForma.Location = new System.Drawing.Point(116, 337);
+            this.cmbForma.Location = new System.Drawing.Point(87, 274);
+            this.cmbForma.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cmbForma.Name = "cmbForma";
-            this.cmbForma.Size = new System.Drawing.Size(121, 24);
+            this.cmbForma.Size = new System.Drawing.Size(92, 21);
             this.cmbForma.TabIndex = 4;
             // 
             // btnSelecionar
             // 
-            this.btnSelecionar.Location = new System.Drawing.Point(116, 380);
+            this.btnSelecionar.Location = new System.Drawing.Point(87, 309);
+            this.btnSelecionar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSelecionar.Name = "btnSelecionar";
-            this.btnSelecionar.Size = new System.Drawing.Size(121, 39);
+            this.btnSelecionar.Size = new System.Drawing.Size(91, 32);
             this.btnSelecionar.TabIndex = 5;
             this.btnSelecionar.Text = "Selecionar";
             this.btnSelecionar.UseVisualStyleBackColor = true;
             // 
             // btnAdicionar
             // 
-            this.btnAdicionar.Location = new System.Drawing.Point(116, 425);
+            this.btnAdicionar.Location = new System.Drawing.Point(87, 345);
+            this.btnAdicionar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAdicionar.Name = "btnAdicionar";
-            this.btnAdicionar.Size = new System.Drawing.Size(121, 57);
+            this.btnAdicionar.Size = new System.Drawing.Size(91, 46);
             this.btnAdicionar.TabIndex = 6;
             this.btnAdicionar.Text = "Adicionar Nova Formula";
             this.btnAdicionar.UseVisualStyleBackColor = true;
             // 
+            // Hiacaco
+            // 
+            this.Hiacaco.AutoSize = true;
+            this.Hiacaco.Location = new System.Drawing.Point(108, 155);
+            this.Hiacaco.Name = "Hiacaco";
+            this.Hiacaco.Size = new System.Drawing.Size(47, 13);
+            this.Hiacaco.TabIndex = 7;
+            this.Hiacaco.Text = "Hiacaco";
+            // 
             // frmPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(372, 509);
+            this.ClientSize = new System.Drawing.Size(279, 414);
+            this.Controls.Add(this.Hiacaco);
             this.Controls.Add(this.btnAdicionar);
             this.Controls.Add(this.btnSelecionar);
             this.Controls.Add(this.cmbForma);
             this.Controls.Add(this.picImage);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmPrincipal";
             this.Text = "Menu";
             ((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
@@ -105,5 +122,6 @@ namespace CalculadoraGeometrica.Forms
         private System.Windows.Forms.ComboBox cmbForma;
         private System.Windows.Forms.Button btnSelecionar;
         private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.Label Hiacaco;
     }
 }
