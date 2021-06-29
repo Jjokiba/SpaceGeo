e88436870a51dbb632a022449a36519f279f4369
Hiago Indalecio <hiagoindalecio@outlook.com>
2021-06-28 22:22:52 -0300

    Atualização form principal.

--- CalculadoraGeometrica/Forms/frmAdicionar.Designer.cs
+++ CalculadoraGeometrica/Forms/frmAdicionar.Designer.cs
@@ -41,7 +41,6 @@
             this.button5 = new System.Windows.Forms.Button();
             this.btnInserirFormula = new System.Windows.Forms.Button();
             this.label8 = new System.Windows.Forms.Label();
-            this.txtNomeFormula = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
             this.btnParentese = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
@@ -58,6 +57,7 @@
             this.txtVar2 = new System.Windows.Forms.TextBox();
             this.txtVar1 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.cmbFormulas = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox2.SuspendLayout();
@@ -74,9 +74,9 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.radioButton1);
             this.groupBox1.Location = new System.Drawing.Point(9, 10);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
             this.groupBox1.Size = new System.Drawing.Size(546, 125);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
@@ -85,7 +85,7 @@
             // button3
             // 
             this.button3.Location = new System.Drawing.Point(274, 46);
-            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 38);
             this.button3.TabIndex = 16;
@@ -96,7 +96,7 @@
             // 
             this.cmbNomeForma.FormattingEnabled = true;
             this.cmbNomeForma.Location = new System.Drawing.Point(15, 64);
-            this.cmbNomeForma.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cmbNomeForma.Margin = new System.Windows.Forms.Padding(2);
             this.cmbNomeForma.Name = "cmbNomeForma";
             this.cmbNomeForma.Size = new System.Drawing.Size(176, 21);
             this.cmbNomeForma.TabIndex = 4;
@@ -104,7 +104,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(194, 28);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(76, 74);
             this.pictureBox1.TabIndex = 15;
@@ -123,7 +123,7 @@
             // txtNomeForma
             // 
             this.txtNomeForma.Location = new System.Drawing.Point(15, 46);
-            this.txtNomeForma.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNomeForma.Margin = new System.Windows.Forms.Padding(2);
             this.txtNomeForma.Name = "txtNomeForma";
             this.txtNomeForma.Size = new System.Drawing.Size(176, 20);
             this.txtNomeForma.TabIndex = 3;
@@ -132,7 +132,7 @@
             // 
             this.radioButton2.AutoSize = true;
             this.radioButton2.Location = new System.Drawing.Point(365, 80);
-            this.radioButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(45, 17);
             this.radioButton2.TabIndex = 2;
@@ -154,7 +154,7 @@
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Location = new System.Drawing.Point(365, 58);
-            this.radioButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(42, 17);
             this.radioButton1.TabIndex = 0;
@@ -164,10 +164,10 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.cmbFormulas);
             this.groupBox2.Controls.Add(this.button5);
             this.groupBox2.Controls.Add(this.btnInserirFormula);
             this.groupBox2.Controls.Add(this.label8);
-            this.groupBox2.Controls.Add(this.txtNomeFormula);
             this.groupBox2.Controls.Add(this.button4);
             this.groupBox2.Controls.Add(this.btnParentese);
             this.groupBox2.Controls.Add(this.button1);
@@ -185,9 +185,9 @@
             this.groupBox2.Controls.Add(this.txtVar1);
             this.groupBox2.Controls.Add(this.label3);
             this.groupBox2.Location = new System.Drawing.Point(9, 140);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
             this.groupBox2.Size = new System.Drawing.Size(546, 273);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
@@ -196,7 +196,7 @@
             // button5
             // 
             this.button5.Location = new System.Drawing.Point(4, 230);
-            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 38);
             this.button5.TabIndex = 18;
@@ -206,7 +206,7 @@
             // btnInserirFormula
             // 
             this.btnInserirFormula.Location = new System.Drawing.Point(466, 230);
-            this.btnInserirFormula.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnInserirFormula.Margin = new System.Windows.Forms.Padding(2);
             this.btnInserirFormula.Name = "btnInserirFormula";
             this.btnInserirFormula.Size = new System.Drawing.Size(75, 38);
             this.btnInserirFormula.TabIndex = 17;
@@ -223,19 +223,11 @@
             this.label8.TabIndex = 16;
             this.label8.Text = "Nome da formula:";
             // 
-            // txtNomeFormula
-            // 
-            this.txtNomeFormula.Location = new System.Drawing.Point(280, 188);
-            this.txtNomeFormula.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.txtNomeFormula.Name = "txtNomeFormula";
-            this.txtNomeFormula.Size = new System.Drawing.Size(246, 20);
-            this.txtNomeFormula.TabIndex = 15;
-            // 
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.Location = new System.Drawing.Point(478, 73);
-            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(46, 39);
             this.button4.TabIndex = 14;
@@ -246,7 +238,7 @@
             // 
             this.btnParentese.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnParentese.Location = new System.Drawing.Point(280, 117);
-            this.btnParentese.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnParentese.Margin = new System.Windows.Forms.Padding(2);
             this.btnParentese.Name = "btnParentese";
             this.btnParentese.Size = new System.Drawing.Size(244, 39);
             this.btnParentese.TabIndex = 13;
@@ -257,7 +249,7 @@
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(428, 73);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(46, 39);
             this.button1.TabIndex = 12;
@@ -268,7 +260,7 @@
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(378, 73);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(46, 39);
             this.button2.TabIndex = 11;
@@ -279,7 +271,7 @@
             // 
             this.btnMais.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMais.Location = new System.Drawing.Point(330, 73);
-            this.btnMais.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnMais.Margin = new System.Windows.Forms.Padding(2);
             this.btnMais.Name = "btnMais";
             this.btnMais.Size = new System.Drawing.Size(46, 39);
             this.btnMais.TabIndex = 10;
@@ -290,7 +282,7 @@
             // 
             this.btnMenos.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMenos.Location = new System.Drawing.Point(280, 73);
-            this.btnMenos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnMenos.Margin = new System.Windows.Forms.Padding(2);
             this.btnMenos.Name = "btnMenos";
             this.btnMenos.Size = new System.Drawing.Size(46, 39);
             this.btnMenos.TabIndex = 2;
@@ -300,7 +292,7 @@
             // txtFormula
             // 
             this.txtFormula.Location = new System.Drawing.Point(280, 50);
-            this.txtFormula.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtFormula.Margin = new System.Windows.Forms.Padding(2);
             this.txtFormula.Name = "txtFormula";
             this.txtFormula.Size = new System.Drawing.Size(246, 20);
             this.txtFormula.TabIndex = 9;
@@ -349,7 +341,7 @@
             // 
             this.cmbNumVar.FormattingEnabled = true;
             this.cmbNumVar.Location = new System.Drawing.Point(15, 49);
-            this.cmbNumVar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cmbNumVar.Margin = new System.Windows.Forms.Padding(2);
             this.cmbNumVar.Name = "cmbNumVar";
             this.cmbNumVar.Size = new System.Drawing.Size(92, 21);
             this.cmbNumVar.TabIndex = 4;
@@ -357,7 +349,7 @@
             // txtVar3
             // 
             this.txtVar3.Location = new System.Drawing.Point(15, 204);
-            this.txtVar3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtVar3.Margin = new System.Windows.Forms.Padding(2);
             this.txtVar3.Name = "txtVar3";
             this.txtVar3.Size = new System.Drawing.Size(176, 20);
             this.txtVar3.TabIndex = 3;
@@ -365,7 +357,7 @@
             // txtVar2
             // 
             this.txtVar2.Location = new System.Drawing.Point(15, 154);
-            this.txtVar2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtVar2.Margin = new System.Windows.Forms.Padding(2);
             this.txtVar2.Name = "txtVar2";
             this.txtVar2.Size = new System.Drawing.Size(176, 20);
             this.txtVar2.TabIndex = 2;
@@ -373,7 +365,7 @@
             // txtVar1
             // 
             this.txtVar1.Location = new System.Drawing.Point(15, 108);
-            this.txtVar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtVar1.Margin = new System.Windows.Forms.Padding(2);
             this.txtVar1.Name = "txtVar1";
             this.txtVar1.Size = new System.Drawing.Size(176, 20);
             this.txtVar1.TabIndex = 1;
@@ -388,6 +380,15 @@
             this.label3.TabIndex = 0;
             this.label3.Text = "Caractere da variavel 1:";
             // 
+            // cmbFormulas
+            // 
+            this.cmbFormulas.FormattingEnabled = true;
+            this.cmbFormulas.Location = new System.Drawing.Point(280, 188);
+            this.cmbFormulas.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbFormulas.Name = "cmbFormulas";
+            this.cmbFormulas.Size = new System.Drawing.Size(244, 21);
+            this.cmbFormulas.TabIndex = 19;
+            // 
             // frmAdicionar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -395,7 +396,7 @@
             this.ClientSize = new System.Drawing.Size(564, 422);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmAdicionar";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmAdicionar";
@@ -438,7 +439,7 @@
         private System.Windows.Forms.Button btnMenos;
         private System.Windows.Forms.Button btnInserirFormula;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox txtNomeFormula;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ComboBox cmbFormulas;
     }
 }
